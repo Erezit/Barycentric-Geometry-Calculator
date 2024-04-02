6d84ffa66550445b33825f8255c5c35b87ca1250
erezit <lopatin.em@phystech.edu>
2024-04-02 17:22:49 +0300

    Add more Textures

--- sourse/ButtonManager.cpp
+++ sourse/ButtonManager.cpp
@@ -2,16 +2,18 @@
 
 
 ButtonManager::ButtonManager() {
-  all_textures.resize(4);
+  all_textures.resize(6);
   all_textures[0].loadFromFile("Textures/Move.png");
   all_textures[2].loadFromFile("Textures/Midpoint.png");
   all_textures[3].loadFromFile("Textures/Line.png");
-  all_buttons.push_back(new Button(10, 20, MoveBasePoint::active, all_textures[0]));
-  all_buttons.push_back(new Button(10, 120, NoMode::active, all_textures[0]));
+  all_textures[4].loadFromFile("Textures/Delete.png"); 
+  all_textures[5].loadFromFile("Textures/PresetOFF.png");
+  all_buttons.push_back(new Button(10, 120, MoveBasePoint::active, all_textures[0]));
+  all_buttons.push_back(new Button(10, 20, NoMode::active, all_textures[5]));
   all_buttons.push_back(new Button(10, 220, CreateMiddlePoint::active, all_textures[2]));
   all_buttons.push_back(new Button(10, 320, CreateLine::active, all_textures[3]));
   all_buttons.push_back(new Button(10, 420, FindLineIntersection::active, all_textures[0]));
-  all_buttons.push_back(new Button(10, 520, RollBack::active, all_textures[0]));
+  all_buttons.push_back(new Button(10, 520, RollBack::active, all_textures[4]));
   all_buttons.push_back(new Button(10, 620, MoveName::active, all_textures[0]));
   all_buttons.push_back(new Button(10, 720, ShowBarycentricCoordinate::active, all_textures[0]));
 }
